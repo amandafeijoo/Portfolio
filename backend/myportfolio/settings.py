d575a1f17c77d8edbffada3a2646f45c1d631abc
Amanda <amanda_feijoo@hotmail.com>
2024-12-08 21:13:59 +0100

    Add deployed URL to ALLOWED_HOSTS, CORS_ALLOWED_ORIGINS, and CSRF_TRUSTED_ORIGINS

--- backend/myportfolio/settings.py
+++ backend/myportfolio/settings.py
@@ -13,7 +13,11 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'portfolio-c6mj.onrender.com',
+]
 
 # Application definition
 
@@ -41,11 +45,13 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  
+    "http://localhost:5173",
+    "https://portfolio-c6mj.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    "https://portfolio-c6mj.onrender.com",
 ]
 
 CSRF_COOKIE_HTTPONLY = False
